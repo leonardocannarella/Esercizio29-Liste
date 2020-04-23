bbff8994ae631cba15519dca4512cc8f28859906
Leonardo Cannarella <leonardocannarella11@gmail.com>
2020-04-23 12:19:16 +0200

    Add files via upload
    - Aggiunti vari metodi

--- src/Festa.java
+++ src/Festa.java
@@ -1,3 +1,4 @@
+
 public class Festa {
     private Nodo head;
     private int elementi;
@@ -5,6 +6,25 @@ public class Festa {
     public Festa() {
         head = null;
         elementi=0;
+    }
+
+    public Festa(Invitato[] in)
+    {
+        int lunghezza = in.length;
+        int i = lunghezza-1;
+
+        inserisciListaAlContrario(in, i);
+    }
+
+    private void inserisciListaAlContrario(Invitato[] in, int i)
+    {
+        inserisciInTesta(in[i]);
+        i--;
+        while(i>=0)
+        {
+            inserisciInCoda(in[i]);
+            i--;
+        }
     }
 
     private Nodo getLinkPosizione(int posizione) throws FestaException {
@@ -101,6 +121,85 @@ public class Festa {
                 elementi--;
             }
             return;
+    }
+
+    public void lastFirst(){
+        if(head==null)
+            return; //lista vuota
+        if(head.getLink()==null)
+            return;//lista con un solo elemento
+
+        Nodo n1=head;
+        head=head.getLink();
+        Nodo n2=head;
+        Nodo n3=head;
+        n1.setLink(null);
+        while (n2.getLink()!=null){
+            n3=n2;
+            n2=n2.getLink();
+        }
+        n3.setLink(n1);
+        n2.setLink(head);
+        head=n2;
+    }
+
+
+    public int shuffle(int k, int h){
+        if(k==h) //stessa posizione
+            return 0;
+        if (head==null)//lista vuota
+            return -1;
+        if (head.getLink()==null) //un solo elemento nella lista
+            return -1;
+        if (k>elementi)
+            return -1; //K è maggiore degli elementi della lista
+        if(((k==1)&&(h==elementi))||((k==elementi)&&(h==1)))
+        {
+            System.out.println("Chiamata la lastFirst()");
+            lastFirst();
+            return 1;
+        }
+        //individuazione del K-esimo nodo
+        Nodo n1=head;
+        Nodo n2=head;
+        int p=1;
+
+        while (p<k){
+            n2=n1;
+            n1=n1.getLink();
+            p++;
+        }
+
+        if (p==1)//primo elemento della lista
+            head=head.getLink();
+        else
+            n2.setLink(n1.getLink()); //si stacca l'elemento in posizione K
+
+
+        //si individua la posizione h
+
+        if(h<=1){
+            n1.setLink(head);
+            head=n1;
+        }
+        else if (h>=elementi){//inserimento in coda
+            n2=head;
+            while (n2.getLink()!=null)
+                n2=n2.getLink();
+            n2.setLink(n1);
+            n1.setLink(null);
+        }
+        else{
+            n2=head;
+            p=1;
+            while (p<h-1){
+                n2=n2.getLink();
+                p++;
+            }
+            n1.setLink(n2.getLink());
+            n2.setLink(n1);
+        }
+        return 1;
     }
 
     private String visita(Nodo p) {
--- src/Test.java
+++ src/Test.java
@@ -2,91 +2,95 @@ import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) throws FestaException {
+
         Invitato i1 = new Invitato("Bianchi Giovanni", 'M', "0586 854822");
         Invitato i2 = new Invitato("Rossi Marta", 'F', "0586 844853");
         Invitato i3 = new Invitato("Neri Marco", 'M', "0586 444722");
         Invitato i4 = new Invitato("Verdi Roberta", 'F', "0586 974824");
         Invitato i5 = new Invitato("Leonardo Cannarella", 'M', "334 9086784");
-        Invitato i6 = new Invitato("Raoul Cannarella", 'M', "345 0453672");
+        Invitato i6 = new Invitato("Mario Rossi", 'M', "345 0453672");
 
-        Festa f = new Festa();
-
-        f.inserisciInTesta(i1);
-        f.inserisciInTesta(i2);
-        f.inserisciInCoda(i3);
-
-        try {
-            f.inserisciInPosizione(i4, 2);
-        } catch (FestaException exception) {
-            System.out.println(exception.getError());
-        }
-
-        System.out.println("Visita ricorsiva: ");
-        System.out.println(f.elenco());
-        System.out.println("------------------");
-        System.out.println(f.toString());
-        System.out.println("------------------");
-
-        try {
-            f.eliminaInPosizione(2);
-        } catch (FestaException exception) {
-            System.out.println(exception.getError());
-        }
+        Invitato [] v = {i1,i2,i3,i4,i5,i6};
+        Festa f = new Festa(v);
 
         System.out.println(f.toString());
-        System.out.println("------------------");
 
-        try {
-            f.eliminaInCoda();
-        } catch (FestaException exception) {
-            System.out.println(exception.getError());
-        }
 
-        System.out.println(f.toString());
-        System.out.println("------------------");
+        /*
+          f.inserisciInTesta(i1);
+                  f.inserisciInTesta(i2);
+                  f.inserisciInCoda(i3);
 
-        try {
-            f.eliminaInTesta();
-        } catch (FestaException exception) {
-            System.out.println(exception.getError());
-        }
+                  try {
+                      f.inserisciInPosizione(i4, 2);
+                  } catch (FestaException exception) {
+                      System.out.println(exception.getError());
+                  }
 
-        System.out.println(f.toString());
-        System.out.println("------------------");
+                  System.out.println("Visita ricorsiva: ");
+                  System.out.println(f.elenco());
+                  System.out.println("------------------");
+                  System.out.println(f.toString());
+                  System.out.println("------------------");
 
-        try {
-            f.eliminaInPosizione(5);
-        } catch (FestaException exception) {
-            System.out.println(exception.getError());
-        }
+                  try {
+                      f.eliminaInPosizione(2);
+                  } catch (FestaException exception) {
+                      System.out.println(exception.getError());
+                  }
 
-        System.out.println(f.toString());
-        System.out.println("------------------");
-        try {
-            f.eliminaInPosizione(1);
-        } catch (FestaException exception) {
-            System.out.println(exception.getError());
-        }
+                  System.out.println(f.toString());
+                  System.out.println("------------------");
 
-        System.out.println(f.toString());
-        System.out.println("------------------");
+                  try {
+                      f.eliminaInCoda();
+                  } catch (FestaException exception) {
+                      System.out.println(exception.getError());
+                  }
 
-        try {
-            f.eliminaInPosizione(1);
-        } catch (FestaException exception) {
-            System.out.println(exception.getError());
-        }
+                  System.out.println(f.toString());
+                  System.out.println("------------------");
 
-        f.inserisciInTesta(i5);
-        f.inserisciInCoda(i6);
-        System.out.println(f.toString());
-        try
-        {
-            f.eliminaInPosizione(2);
-        } catch (FestaException ex)
-        {
-            System.out.println(ex.getError());
-        }
-        System.out.println(f.toString());
+                  try {
+                      f.eliminaInTesta();
+                  } catch (FestaException exception) {
+                      System.out.println(exception.getError());
+                  }
+
+                  System.out.println(f.toString());
+                  System.out.println("------------------");
+
+                  try {
+                      f.eliminaInPosizione(5);
+                  } catch (FestaException exception) {
+                      System.out.println(exception.getError());
+                  }
+
+                  System.out.println(f.toString());
+                  System.out.println("------------------");
+                  try {
+                      f.eliminaInPosizione(1);
+                  } catch (FestaException exception) {
+                      System.out.println(exception.getError());
+                  }
+
+                  System.out.println(f.toString());
+                  System.out.println("------------------");
+
+                  try {
+                      f.eliminaInPosizione(1);
+                  } catch (FestaException exception) {
+                      System.out.println(exception.getError());
+                  }
+
+                  f.inserisciInTesta(i5);
+                  f.inserisciInCoda(i6);
+                  f.inserisciInPosizione(i2, 1);
+                  System.out.println(f.toString());
+
+                  System.out.println(f.shuffle(3,1));
+                  System.out.println(f.toString());
+              }
+          */
     }
 }
